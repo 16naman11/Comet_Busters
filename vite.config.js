9abc213d3fbe38bd92c1b421c0d01ab20f7b572e
Naman Shah <141851295+16naman11@users.noreply.github.com>
2024-07-11 12:27:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Comet_Busters/', 
-  css: {
+  css:{
     modules: {
-      localsConvention: "camelCase",
+      localsConvention: 'camelCase',
     },
   },
-})
+});
